44ea9a46d56083634f9be6138602902403b85e0c
Sneha1b <smmadle235@gmail.com>
2018-09-17 09:14:12 -0400

    remote ver 3

--- remote ver 3.0/client/files/fib.cpp
+++ remote ver 3.0/client/files/fib.cpp
@@ -19,7 +19,7 @@ struct fib<0>{
 
 int main(){
 	cout << "Fib of 5 is:" << fib<5>::value <<endl;
-	cout << "Fib of 6 is:" << fib<6>::value <<endl;
+	cout << "Fib of 6 is:" << fib<6>::value <<endl
 	cout << "Fib of 7 is:" << fib<7>::value <<endl;
 	return 0;
 }
