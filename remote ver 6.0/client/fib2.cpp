baa7561ca56fe8575612931567989bc5ebaa9bfc
Sneha1b <smmadle235@gmail.com>
2018-09-17 09:18:16 -0400

    remote ver 6.0

---
+++ remote ver 6.0/client/fib2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+template <unsigned N>
+struct fib{
+	static const int value = fib<N-1>::value + fib<N-2>::value;
+};
+
+template <>
+struct fib<1>{
+	static const int value = 1;
+};
+
+template <>
+struct fib<0>{
+	static const int value = 1;
+};
+
+int main(){
+	cout << "Fib of 5 is:" << fib<5>::value <<endl;
+	cout << "Fib of 6 is:" << fib<6>::value <<endl;
+	cout << "Fib of 7 is:" << fib<7>::value <<endl;
+	return 0;
+}
---
+++ remote ver 6.0/server/Server.java
@@ -0,0 +1,159 @@
+import java.io.*;
+import java.net.*;
+import java.nio.file.*;
+import java.util.*;
+
+public class Server{
+	private static final int maxClientsCount = 10;
+	private static final clientThread[] threads = new clientThread[maxClientsCount];
+	private static String prompt = "Press";
+
+	public static void main(String[] argv){
+	    try{
+	    	// The default port.
+		    int portNumber = 10000;
+		    // The default host.
+		    String host = "127.0.0.1";
+
+		    if (argv.length < 1) {
+		    	System.out.println("Usage: java Server <host ip>");
+		    }
+		    else{
+		    	host = argv[0];
+		    }
+		    System.out.println("Now connecting to MainServer at " + host + ":" + portNumber);
+			Socket sock = new Socket(host, portNumber);
+
+			int numServices = 0;
+
+			Scanner in = new Scanner(System.in);
+			DataOutputStream dos = new DataOutputStream(sock.getOutputStream());
+			int choice;
+			String service;
+
+			dos.writeInt(0);
+			dos.flush();
+
+			while(true){
+				System.out.println("Enter the name of the service (\"exit\" to complete registering):");
+				service = in.next();
+				// System.out.println(service);
+				dos.writeUTF(service);
+				dos.flush();
+				if(service.equals("exit")) break;
+				prompt = (prompt + " " + numServices + ": For " + service + " |");
+				numServices++;	
+			}
+
+			in.close();
+			dos.close();
+			sock.close();	
+			//End connection with mainserver
+
+			//Start hosting the service
+	    	ServerSocket servsock = new ServerSocket(9999);
+	    	while(true){
+	    		Socket clientSocket = servsock.accept();
+		        int i = 0;
+		        for (i = 0; i < maxClientsCount; i++) {
+		        	if (threads[i] == null) {
+		        		(threads[i] = new clientThread(clientSocket, threads)).start();
+		            	break;
+		          	}
+		        }
+		        if (i == maxClientsCount) {
+		        	PrintStream os = new PrintStream(clientSocket.getOutputStream());
+			        os.println("Server too busy. Try later.");
+			        os.close();
+			        clientSocket.close();
+		        }
+	    	}
+	    } catch(IOException e){
+	    		System.out.println(e);
+	    }
+	}
+}
+
+
+
+// import java.io.*;
+// import java.net.*;
+// import java.nio.file.*;
+// import java.util.*;
+
+// public class Server{
+// 	private static final int maxClientsCount = 10;
+// 	private static final clientThread[] threads = new clientThread[maxClientsCount];
+// 	private static String prompt = "Press";
+
+// 	public static void main(String[] argv){
+// 	    try{
+// 	    	// The default port.
+// 		    int portNumber = 10000;
+// 		    // The default host.
+// 		    String host = "127.0.0.1";
+
+// 		    if (argv.length < 1) {
+// 		    	System.out.println("Usage: java Server <host ip>");
+// 		    }
+// 		    else{
+// 		    	host = argv[0];
+// 		    }
+// 		    System.out.println("Now connecting to MainServer at " + host + ":" + portNumber);
+// 			Socket sock = new Socket(host, portNumber);
+
+// 			int numServices = 0;
+
+// 			Scanner in = new Scanner(System.in);
+// 			DataOutputStream dos = new DataOutputStream(sock.getOutputStream());
+// 			int choice;
+// 			String service;
+
+// 			dos.writeInt(0);
+// 			dos.flush();
+
+// 			while(true){
+// 				System.out.println("Enter the name of the service (\"exit\" to complete registering):");
+// 				service = in.next();
+// 				// System.out.println(service);
+// 				dos.writeUTF(service);
+// 				dos.flush();
+// 				if(service.equals("exit")) break;
+// 				prompt = (prompt + " " + numServices + ": For " + service + " |");
+// 				numServices++;	
+// 			}
+
+// 			dos.close();
+// 			sock.close();	
+// 			//End connection with mainserver
+
+// 			//Start hosting the service
+// 	    	ServerSocket servsock = new ServerSocket(9999);
+// 	    	while( !( in.hasNext() &&  in.next().equals("exit") ) ){
+// 	    		Socket clientSocket = servsock.accept();
+// 		        int i = 0;
+// 		        for (i = 0; i < maxClientsCount; i++) {
+// 		        	if (threads[i] == null) {
+// 		        		(threads[i] = new clientThread(clientSocket, threads)).start();
+// 		            	break;
+// 		          	}
+// 		        }
+// 		        if (i == maxClientsCount) {
+// 		        	PrintStream os = new PrintStream(clientSocket.getOutputStream());
+// 			        os.println("Server too busy. Try later.");
+// 			        os.close();
+// 			        clientSocket.close();
+// 		        }
+// 	    	}
+// 	    	int i = 0;
+// 	        for (i = 0; i < maxClientsCount; i++) {
+// 	        	if (threads[i] != null) {
+// 	        		threads[i].join();
+// 	          	}
+// 	        }
+// 	    	in.close();
+// 	    } catch(Exception e){
+// 	    		System.out.println(e);
+// 	    }
+// 	}
+// }
